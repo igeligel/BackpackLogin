90435257b6aed68db098503feff9e5d7bdba40d8
igeligel <kevinigeligeligel@gmail.com>
2015-12-11 18:08:47 +0100

    Edited Start of the program.

--- BackpackLogin/Main/Program.cs
+++ BackpackLogin/Main/Program.cs
@@ -3,12 +3,25 @@ using BackpackLogin.API;
 
 namespace BackpackLogin.Main
 {
-    class Program
+    /// <summary>
+    /// Program class to initiate the program.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main method of the program.
+        /// </summary>
+        /// <param name="args">Arguments which will be provided. There is no support for that right now.</param>
+        private static void Main(string[] args)
         {
+            if (args == null) throw new ArgumentNullException(nameof(args));
             Console.ReadKey();
+            // Create instance of the login.
             var backpackLogin = new Login();
+            // Login with two factor code.
+            backpackLogin.DoLogin("steamusername", "steampasswort", "twofactorcode");
+            // Login without two factor code.
+            backpackLogin.DoLogin("steamusername", "steampassword");
             Console.ReadKey();
         }
     }
