99ff3f74c2e89d0cf35cfe60e7afa7606001ee6b
Kevin Peters <git.igeligel@googlemail.com>
2017-04-23 03:24:21 +0200

    refactor(example)
    refactor example a bit

--- src/BackpackLoginExample/Program.cs
+++ src/BackpackLoginExample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using BackpackLoginExample.Settings;
 using HedgehogSoft.BackpackLogin;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BackpackLoginExample
 {
@@ -9,15 +11,16 @@ namespace BackpackLoginExample
         // ReSharper disable once UnusedMember.Local
         private static void Main()
         {
+            Console.WriteLine("Press a key to start login process.");
             Console.ReadKey();
             Loader.LoadSettings();
-            Console.WriteLine($"Username {ConsoleSettings.Instance["username"]}");
+            Console.WriteLine($"{ConsoleSettings.Instance["username"]} will login to backpack.tf");
             var backpackLoginClient = new BackpackLoginClient();
-            backpackLoginClient.Login(
+            var cookieContainer = backpackLoginClient.Login(
                 ConsoleSettings.Instance["username"],
                 ConsoleSettings.Instance["password"],
                 ConsoleSettings.Instance["sharedSecret"]);
-            Console.ReadKey();
+            Console.WriteLine($"Login successful. Found {cookieContainer.Count} Cookies.");
         }
     }
 }
